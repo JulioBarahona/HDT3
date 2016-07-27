d059352f95520c5e4a26caa51b29ded081fea094
JulioBarahona <julio.fbm96@gmail.com>
2016-07-27 12:13:54 -0600

    Add files via upload

--- generadorText.java
+++ generadorText.java
@@ -1,6 +1,7 @@
 import java.awt.List;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
 
 /**
@@ -12,14 +13,20 @@ import java.util.Arrays;
  * 
  * @version 07.27.2016
  */
-public static void main(String[] args){
-	
-	String nombreArchivo = "secuenciaNumeros.txt";
+
+public class generadorText {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        // TODO code application logic here
+        String nombreArchivo = "secuenciaNumeros.txt";
 	try {
-		PrintWriter archivoGenerado = new PrintWriter("numerosRandom.txt", "UTF-8");
-		outputStream.println("test");
-		outputStream.close();
-		System.out.println("testt");
+		PrintWriter textFile = new PrintWriter("numerosRandom.txt", "UTF-8");
+                    textFile.println("test");
+                    textFile.close();
+                    System.out.println("test");
 		}
 	catch (FileNotFoundException e){
 		e.printStackTrace();
@@ -28,4 +35,4 @@ public static void main(String[] args){
 
 	
 	
-}
+    }
