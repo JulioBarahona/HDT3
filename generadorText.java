c44e3ee1ba7b9fbe9562309dc0a7facc57b2f541
JulioBarahona <julio.fbm96@gmail.com>
2016-07-27 11:23:52 -0600

    Random
    Genera numeros y hacer archivo

---
+++ generadorText.java
@@ -0,0 +1,15 @@
+import java.io.PrintWriter;
+
+/**
+
+    * Ejercicio en clase semana 3 AET
+    * @author Julio Barahona M
+	* @version 07.27.2016
+
+*/
+public class generadorText {
+	PrintWriter writer = new PrintWriter("numerosRandom.txt", "UTF-8");
+	
+
+	public void Genera{}
+	}	
